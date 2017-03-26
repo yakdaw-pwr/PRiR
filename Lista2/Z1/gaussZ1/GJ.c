d7365e15ca99c6ecc3587e57d2459231c38bf5e9
Unknown <kamajabu@gmail.com>
2017-03-27 00:29:35 +0200

    Fixing order of fuctions, which apparently matter

--- Lista2/Z1/gaussZ1/GJ.c
+++ Lista2/Z1/gaussZ1/GJ.c
@@ -6,6 +6,39 @@
 #include "displayFunctions.h"
 #include "mathFunctions.h"
 
+double* CalculateGJ(double** matrix, int rowCount){
+  double* solutionMatrix = (double*) calloc(rowCount, sizeof(double)); 
+  
+  for(int i = 0; i < rowCount; i++){
+    if(matrix[i][i] == 0){
+	int newRow = FindNonZeroElement(matrix, rowCount, i);
+	SwitchRows(matrix[i], matrix[newRow], rowCount+1);
+    }
+    DivideRowBy(matrix[i], matrix[i][i], rowCount+1);
+    SubRows(matrix, i, rowCount);
+  }
+  
+  for(int i = 0; i< rowCount; i++){
+    solutionMatrix[i] = matrix[i][rowCount];
+  }
+  return solutionMatrix;
+}
+
+void ReleaseMemory(double** matrix, double* solution, int rowCount){
+ for(int i = 0; i < rowCount; i++){
+  free(matrix[i]);
+ }
+ free(matrix);
+ free(solution);
+}
+
+void Calculate(const char* fileName, double** matrix, double* solution, int* rowCount){
+ matrix = ReadData(fileName, matrix, rowCount);
+ solution = CalculateGJ(matrix, *rowCount);
+ PrintFinalMatrix(matrix,*rowCount);
+ ReleaseMemory(matrix, solution, *rowCount);
+}
+
 int main(){
         //creating two dimensional matrix
 	double** matrix;
@@ -20,54 +53,20 @@ int main(){
 	Calculate("dane1", matrix, solution, &rowCount);
        
         //getting the time
-	double timeMiddle = clock() / (CLOCKS_PER_SEC / 1000000);
+	double firstDataTime = clock() / (CLOCKS_PER_SEC / 1000000);
         
 	printf("\nSecond data\n");
 	Calculate("dane2", matrix, solution, &rowCount);
 	
         printf("\n");
-	double timeEnd = clock() / (CLOCKS_PER_SEC / 1000000);
+	double finishTime = clock() / (CLOCKS_PER_SEC / 1000000);
 	
         printf("First data time: %.6lf\n"
                 "Second data time: %.6lf\n"
 		"Complete time: %.6lf\n", 
-                (timeMiddle-timeStart)/1000000, 
-                (timeEnd - timeMiddle)/1000000, 
-                (timeEnd-timeStart)/1000000);
+                (firstDataTime - timeStart)/1000000, 
+                (finishTime - firstDataTime)/1000000, 
+                (finishTime-timeStart)/1000000);
 	
 return(0);
 }
-
-double* CalculateGJ(double** matrix, int rowCount){
-  double* x = (double*) calloc(rowCount, sizeof(double)); // solution
-  
-  for(int i = 0; i < rowCount; i++){
-    if(matrix[i][i] == 0){
-	int newRow = FindNonZeroElement(matrix, rowCount, i);
-	SwitchRows(matrix[i], matrix[newRow], rowCount+1);
-    }
-    DivideRowBy(matrix[i], matrix[i][i], rowCount+1);
-    SubRows(matrix, i, rowCount);
-  }
-  
-  for(int i = 0; i< rowCount; i++){
-    x[i] = matrix[i][rowCount];
-  }
-  return x;
-}
-
-void Calculate(const char* fileName, double** matrix, double* solution, int* rowCount){
- matrix = ReadData(fileName, matrix, rowCount);
- solution = CalculateGJ(matrix, *rowCount);
- PrintFinalMatrix(matrix,*rowCount);
- ReleaseMemory(matrix, solution, *rowCount);
-}
-
-void ReleaseMemory(double** matrix, double* solution, int rowCount){
- for(int i = 0; i < rowCount; i++){
-  free(matrix[i]);
- }
- free(matrix);
- free(solution);
-}
-
