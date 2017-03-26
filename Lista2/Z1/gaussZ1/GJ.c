e74a621e6daacfdf21094e28d2c6ca48afe75062
Unknown <kamajabu@gmail.com>
2017-03-27 00:24:05 +0200

    More code cleaning

--- Lista2/Z1/gaussZ1/GJ.c
+++ Lista2/Z1/gaussZ1/GJ.c
@@ -6,61 +6,65 @@
 #include "displayFunctions.h"
 #include "mathFunctions.h"
 
-int main()
-{
+int main(){
+        //creating two dimensional matrix
 	double** matrix;
         int rowCount = 0;
 	double* solution;
+        
+        //starting the time and choosing accuracy
 	double timeStart = clock() / (CLOCKS_PER_SEC / 1000000);
-	printf("\nDane 1\n");
+	
+        printf("\nFirst data\n");
+        //calculating first data
 	Calculate("dane1", matrix, solution, &rowCount);
+       
+        //getting the time
 	double timeMiddle = clock() / (CLOCKS_PER_SEC / 1000000);
-	printf("\nDane 2\n");
+        
+	printf("\nSecond data\n");
 	Calculate("dane2", matrix, solution, &rowCount);
-	printf("\n");
+	
+        printf("\n");
 	double timeEnd = clock() / (CLOCKS_PER_SEC / 1000000);
-	printf("Pierwszy zestaw danych: %.6lf\nDrugi zestaw danych: %.6lf\n"
-		"Lacznie: %.6lf\n", (timeMiddle-timeStart)/1000000, (timeEnd - timeMiddle)/1000000, (timeEnd-timeStart)/1000000);
+	
+        printf("First data time: %.6lf\n"
+                "Second data time: %.6lf\n"
+		"Complete time: %.6lf\n", 
+                (timeMiddle-timeStart)/1000000, 
+                (timeEnd - timeMiddle)/1000000, 
+                (timeEnd-timeStart)/1000000);
 	
 return(0);
 }
 
-double* CalculateGJ(double** matrix, int rowCount)
-{
+double* CalculateGJ(double** matrix, int rowCount){
   double* x = (double*) calloc(rowCount, sizeof(double)); // solution
-  int i;
-  for(i = 0; i < rowCount; i++)
-  {
-    if(matrix[i][i] == 0)
-    {
+  
+  for(int i = 0; i < rowCount; i++){
+    if(matrix[i][i] == 0){
 	int newRow = FindNonZeroElement(matrix, rowCount, i);
 	SwitchRows(matrix[i], matrix[newRow], rowCount+1);
     }
     DivideRowBy(matrix[i], matrix[i][i], rowCount+1);
     SubRows(matrix, i, rowCount);
   }
-  for(i = 0; i< rowCount; i++)
-  {
+  
+  for(int i = 0; i< rowCount; i++){
     x[i] = matrix[i][rowCount];
   }
   return x;
 }
 
-
-
-void Calculate(const char* fileName, double** matrix, double* solution, int* rowCount)
-{
+void Calculate(const char* fileName, double** matrix, double* solution, int* rowCount){
  matrix = ReadData(fileName, matrix, rowCount);
  solution = CalculateGJ(matrix, *rowCount);
  PrintFinalMatrix(matrix,*rowCount);
  ReleaseMemory(matrix, solution, *rowCount);
 }
 
-void ReleaseMemory(double** matrix, double* solution, int rowCount)
-{
- int i;
- for(i = 0; i < rowCount; i++)
- {
+void ReleaseMemory(double** matrix, double* solution, int rowCount){
+ for(int i = 0; i < rowCount; i++){
   free(matrix[i]);
  }
  free(matrix);
