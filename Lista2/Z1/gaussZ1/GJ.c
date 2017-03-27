4ca0bd240441ec514c8d4d8cc5591b49b35320b6
Unknown <kamajabu@gmail.com>
2017-03-27 18:35:00 +0200

    Start on saving file

--- Lista2/Z1/gaussZ1/GJ.c
+++ Lista2/Z1/gaussZ1/GJ.c
@@ -7,45 +7,46 @@
 #include "mathFunctions.h"
 #include "GJ.h"
 
-double* calculateGJ(double** matrix, int rowCount){
-  double* solutionMatrix = (double*) calloc(rowCount, sizeof(double)); 
-  
-  for(int i = 0; i < rowCount; i++){
-    if(matrix[i][i] == 0){
-	int newRow = findNonZeroElement(matrix, rowCount, i);
-	switchRows(matrix[i], matrix[newRow], rowCount+1);
+double* calculateGJ(double** matrix, int rowCount) {
+    double* solutionMatrix = (double*) calloc(rowCount, sizeof (double));
+
+    for (int i = 0; i < rowCount; i++) {
+        if (matrix[i][i] == 0) {
+            int newRow = findNonZeroElement(matrix, rowCount, i);
+            switchRows(matrix[i], matrix[newRow], rowCount + 1);
+        }
+        //jedynki na srodku
+        divide(matrix[i], matrix[i][i], rowCount + 1);
+        //    printf("Next step\n");
+        //    printMatrix(matrix, rowCount);
+        //    printf("\n");
+        subRows(matrix, i, rowCount);
     }
-    //jedynki na srodku
-    divide(matrix[i], matrix[i][i], rowCount+1);
-//    printf("Next step\n");
-//    printMatrix(matrix, rowCount);
-//    printf("\n");
-    subRows(matrix, i, rowCount);
-  }
-  
-  for(int i = 0; i< rowCount; i++){
-    solutionMatrix[i] = matrix[i][rowCount];
-  }
-  return solutionMatrix;
+
+    for (int i = 0; i < rowCount; i++) {
+        solutionMatrix[i] = matrix[i][rowCount];
+    }
+    return solutionMatrix;
 }
 
-void releaseMemory(double** matrix, double* solution, int rowCount){
- for(int i = 0; i < rowCount; i++){
-  free(matrix[i]);
- }
- free(matrix);
- free(solution);
+void releaseMemory(double** matrix, double* solution, int rowCount) {
+    for (int i = 0; i < rowCount; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+    free(solution);
 }
 
-void calculate(const char* fileName){ 
- //creating two dimensional matrix
- double** matrix;
- double* solution;
- int rowCount = 0;
+void calculate(const char* fileName) {
+    //creating two dimensional matrix
+    double** matrix;
+    double* solution;
+    int rowCount = 0;
 
- matrix = loadMatrix(fileName, matrix, &rowCount);
- solution = calculateGJ(matrix, rowCount);
- printFinalMatrix(matrix,rowCount);
- releaseMemory(matrix, solution, rowCount);
+    matrix = loadMatrix(fileName, matrix, &rowCount);
+    solution = calculateGJ(matrix, rowCount);
+    printFinalMatrix(matrix, rowCount);
+    saveFinalMatrix(matrix, rowCount);
+    releaseMemory(matrix, solution, rowCount);
 }
 
