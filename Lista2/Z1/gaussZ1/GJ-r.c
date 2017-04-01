79933154d633807203fffae9c1c7aa0d4c518f00
Unknown <kamajabu@gmail.com>
2017-04-01 17:18:42 +0200

    Y0

--- Lista2/Z1/gaussZ1/GJ-r.c
+++ Lista2/Z1/gaussZ1/GJ-r.c
@@ -39,21 +39,19 @@ double** loadMatrix(const char* file_name, double** matrix, int* rowCount) {
 //math
 
 void divide(double* row, double valueOfMiddleElement, int rowSize) {
-#pragma omp parallel for
     for (int i = 0; i < rowSize; i++) {
         row[i] = row[i] / valueOfMiddleElement;
     }
 }
 
 void substract(double* baseRow, double* destRow, double multiValue, int rowSize) {
-#pragma omp parallel for
     for (int i = 0; i < rowSize; i++) {
         destRow[i] = destRow[i] - (baseRow[i] / multiValue);
     }
 }
 
 void subRows(double** matrix, int rowIndex, int rowCount) {
-#pragma omp parallel for
+#pragma omp parallel for num_threads(10)
     for (int i = 0; i < rowCount; i++) {
         if (i != rowIndex && matrix[i][rowIndex] != 0) {
             double value = matrix[rowIndex][rowIndex] / matrix[i][rowIndex];
