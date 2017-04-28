344ecb95cd9e93c786c866c81670bdce198a6832
Unknown <200412@student.pwr.edu.pl>
2017-04-28 04:20:07 +0200

    Kamil pewnie teraz wstaje

--- Lista 7/GJ-MPI/main.c
+++ Lista 7/GJ-MPI/main.c
@@ -24,112 +24,218 @@ void appendWithIdentityMatrix(double** matrix, int size);
 void calculateInverseMatrix(double** m, int size);
 
 int main(int argc, char** argv) {
-    
-    // Start zliczania czasu
-    double start = MPI_Wtime();
 
-    // Sprawdzenie poprawnosci argumentow
-    if (validateExecutionArguments(argc) == 1) {
-        return (1);
-    }
+    // Tags: 1 - matrixSize , 2 - startRowNum , 3 - endRowNum
 
-    char* filePath = argv[1];
+    int worldRank;
 
-    // Sprawdzenie czy plik jest do odczytu
-    if (checkIfFileIsReadable(filePath) == 1) {
-        fprintf(stderr, "File not readable.\n");
-        return (1);
-    }
+    int rowSize = 0;
 
-    // Wczytanie macierzy z pliku
-    int rowSize;
-    double** matrix = loadMatrix(filePath, &rowSize);
-
-    printf("Row size: %d\n", rowSize);
-
-    double determinant = calculateMatrixDeterminant(matrix);
-    if (determinant == 0) {
-        fprintf(stderr, "Wczytana macierz jest osobliwa (wyznacznik = 0).\n");
-        return (1);
-    }
-
-    printf("determinant : %lf\n", determinant);
-
-    appendWithIdentityMatrix(matrix, rowSize);
-    
-    
-    
-    
-    
     // Inicjalizacja MPI
-    int rc = MPI_Init(&argc,&argv);
-    
+    MPI_Status status;
+    int rc = MPI_Init(&argc, &argv);
     if (rc != MPI_SUCCESS) {
         fprintf(stderr, "Error starting MPI program.\n");
         MPI_Abort(MPI_COMM_WORLD, rc);
+        return (1);
     }
-    
-    // Pobierz licze procesow
-    int worldSize;
-    MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
-    printf("Processes: %d\n", worldSize);
 
     // Identyfikator procesu
-    int worldRank;
     MPI_Comm_rank(MPI_COMM_WORLD, &worldRank);
-    printf("Process rank: %d\n", worldRank);
-    
-    // Przypadek jeżeli ilość procesów > wielkość macierzy
-    int rowsPerProcess = 1;
-    int rowsForMaster = 0;
-    int activeProcesses = rowSize;
-    
-    if (worldSize < rowSize) {
-         rowsPerProcess = rowSize / worldSize;
-         rowsForMaster = rowSize % worldSize;
-         activeProcesses = worldSize;
-    }
-    printf("Rows per process: %d\n", rowsPerProcess);
-    printf("Rows for master process: %d\n", rowsForMaster);
-    
-    int* startRowNumber = (int*) calloc(worldSize, sizeof (int));
-    int* endRowNumber = (int*) calloc(worldSize, sizeof (int));
-    
-    int i;
-    for (i = 0; i < activeProcesses; i++) {
-        startRowNumber[i] = i * rowsPerProcess;
-        endRowNumber[i] = startRowNumber[i] + rowsPerProcess - 1;
-        
-        printf("--------------\nProcess %d\n", i);
-        printf("start: %d, end: %d\n", startRowNumber[i], endRowNumber[i]);
-        printf("--------------\n");
-    }
-    
-    int masterStartRow = 0;
-    if (rowsForMaster != 0) {
-        masterStartRow = rowSize - rowsForMaster;
-    }
-    
-    printf("Master start row: %d\n", masterStartRow);
-     
-    
-    
-    
-    
-    calculateInverseMatrix(matrix, rowSize);
 
-    int j;
-//    for (i = 0; i < rowSize; i++) {
-//        for (j = 0; j < rowSize; j++) {
-//            printf("%lf\t", matrix[i][j+rowSize]);
+    // Dla procesu MASTER
+    if (worldRank == 0) {
+
+        // Sprawdzenie poprawnosci argumentow
+        if (validateExecutionArguments(argc) == 1) {
+            MPI_Abort(MPI_COMM_WORLD, rc);
+            return (1);
+        }
+
+        char* filePath = argv[1];
+
+        // Sprawdzenie czy plik jest do odczytu
+        if (checkIfFileIsReadable(filePath) == 1) {
+            fprintf(stderr, "File not readable.\n");
+            MPI_Abort(MPI_COMM_WORLD, rc);
+            return (1);
+        }
+
+        // Wczytanie macierzy z pliku
+        double** matrix = loadMatrix(filePath, &rowSize);
+
+        printf("Row size: %d\n", rowSize);
+
+        double determinant = calculateMatrixDeterminant(matrix);
+        if (determinant == 0) {
+            fprintf(stderr, "Wczytana macierz jest osobliwa (wyznacznik = 0).\n");
+            MPI_Abort(MPI_COMM_WORLD, rc);
+            return (1);
+        }
+        printf("determinant : %lf\n", determinant);
+
+        appendWithIdentityMatrix(matrix, rowSize);
+
+        // Pobierz licze procesow
+        int worldSize;
+        MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
+        printf("Processes: %d\n", worldSize);
+
+        // Jest tylko proces master -> wykonaj algorytm sam
+        if (worldSize == 1) {
+            calculateInverseMatrix(matrix, rowSize);
+        } else {
+            // Domyslny przypadek jeżeli ilość procesów > wielkość macierzy
+            int rowsPerProcess = 1;
+            int rowsForMaster = 0;
+            int activeProcesses = rowSize;
+
+            if (worldSize < rowSize) {
+                rowsPerProcess = rowSize / worldSize;
+                rowsForMaster = rowSize % worldSize;
+                activeProcesses = worldSize;
+            }
+            printf("Rows per process: %d\n", rowsPerProcess);
+            printf("Rows for master process: %d\n", rowsForMaster);
+
+            int* startRowNumbers = (int*) calloc(worldSize, sizeof (int));
+            int* endRowNumbers = (int*) calloc(worldSize, sizeof (int));
+
+            int i;
+            for (i = 0; i < activeProcesses; i++) {
+                startRowNumbers[i] = i * rowsPerProcess;
+                endRowNumbers[i] = startRowNumbers[i] + rowsPerProcess - 1;
+            }
+
+            int masterStartRow = 0;
+            if (rowsForMaster != 0) {
+                masterStartRow = rowSize - rowsForMaster;
+            }
+
+            printf("Master start row: %d\n", masterStartRow);
+
+            for (i = 1; i < activeProcesses; i++) {
+                MPI_Send(&rowSize, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
+                MPI_Send(&startRowNumbers[i], 1, MPI_INT, i, 2, MPI_COMM_WORLD);
+                MPI_Send(&endRowNumbers[i], 1, MPI_INT, i, 3, MPI_COMM_WORLD);
+            }
+            
+            // Tymczasowe testy
+            // *(*(data + i) + j) data[i][j]
+//            MPI_Send(startRowNumbers + 1, 3, MPI_INT, 1, 10, MPI_COMM_WORLD);
+            MPI_Send(*(matrix + 3) + 1, 3, MPI_DOUBLE, 1, 10, MPI_COMM_WORLD);
+                        
+            
+
+            int j, k;
+
+            // Pętla główna (k = 0, ..., N-1)
+            for (k = 0; k < rowSize; k++) {
+                // Normalizacja wiersza
+                for (j = rowSize * 2 - 1; j >= k; j--) {
+                    matrix[k][j] = matrix[k][j] / matrix[k][k];
+                }
+
+                // Wysłać do wykonawców [k][k+1] -> [k][2N -1]
+                for (i = 1; i < activeProcesses; i++) {
+//                    MPI_Send(, 1, MPI_INT, i, 10, MPI_COMM_WORLD);
+//                    MPI_Send(&rowSize, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
+//                    MPI_Send(&startRowNumbers[i], 1, MPI_INT, i, 2, MPI_COMM_WORLD);
+//                    MPI_Send(&endRowNumbers[i], 1, MPI_INT, i, 3, MPI_COMM_WORLD);
+                }
+               
+
+                // Wysłać do wykonawców [poczatek -> koniec][k+1] -> [p -> k][2n-1] 
+                
+                // Zerowanie dla #0
+
+                // Odbieranie wyników i aktualizacja macierzy
+
+                // Zerowanie w niepełnym bloku
+
+                //            // usuwanie zer poza przekatna
+                //            for (i = 0; i < rowSize; i++) {
+                //                // i = nr wiersza
+                //                if (i != k) {
+                //                    // j = nr kolumny
+                //                    for (j = rowSize * 2 - 1; j >= k; j--)
+                //                        matrix[i][j] = matrix[i][j] - matrix[i][k] * matrix[k][j];
+                //                }
+                //            }
+
+
+            }
+
+            // Czekaj na komunikaty    
+        }
+
+
+
+        // Wyprowadz wyniki odwracania macierzy
+//        int i, j;
+//        for (i = 0; i < rowSize; i++) {
+//            for (j = 0; j < rowSize; j++) {
+//                printf("%lf\t", matrix[i][j + rowSize]);
+//            }
+//            printf("\n");
 //        }
-//        printf("\n");
-//    }
+    }
+        // Dla procesów wykonawczych
+    else {
+
+        int startRowNumber;
+        int endRowNumber;
+
+        printf("-----------------------");
+        printf("Proces %d: czekam, obecnie %d\n", worldRank, rowSize);
+
+        MPI_Recv(&rowSize, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+        MPI_Recv(&startRowNumber, 1, MPI_INT, 0, 2, MPI_COMM_WORLD, &status);
+        MPI_Recv(&endRowNumber, 1, MPI_INT, 0, 3, MPI_COMM_WORLD, &status);
+
+        printf("Proces %d: doszlo, obecnie %d\n", worldRank, rowSize);
+        printf("Proces %d: start, obecnie %d\n", worldRank, startRowNumber);
+        printf("Proces %d: end, obecnie %d\n", worldRank, endRowNumber);
+        printf("-----------------------");
+        
+        double* normalizedRow = (double*) calloc(rowSize * 2, sizeof (double));
+        double* tempRow = (double*) calloc(rowSize * 2, sizeof (double));
+        
+//        int* blow = (int*) calloc(rowSize * 2, sizeof (int));
+        
+        // Testy
+//        MPI_Recv(blow, 3, MPI_INT, 0, 10, MPI_COMM_WORLD, &status);
+        MPI_Recv(normalizedRow, 3, MPI_DOUBLE, 0, 10, MPI_COMM_WORLD, &status);
+        
+        int i;
+        printf("Wiersz:\n");
+        for (i = 0; i < 5; i++) {
+            printf("%lf\t", normalizedRow[i]);
+        }
+        printf("\n");
+
+        // Odbierz [k][k+1] -> [k][2N -1]
+
+        // Odbierz [poczatek -> koniec][k+1] -> [p -> k][2n-1]
 
 
+        //        for (i = 0; i < rowSize; i++) {
+        //                // i = nr wiersza
+        //                if (i != k) {
+        //                    // j = nr kolumny
+        //                    for (j = rowSize * 2 - 1; j >= k; j--)
+        //                        matrix[i][j] = matrix[i][j] - matrix[i][k] * matrix[k][j];
+        //                }
+        //        }
+
+        // Zwroc prawe czesci swoich wierszow
+    }
+
+
+    //    double start = MPI_Wtime();
 
     //    double stop = MPI_Wtime();
-    
+
     MPI_Finalize();
 
     return (EXIT_SUCCESS);
