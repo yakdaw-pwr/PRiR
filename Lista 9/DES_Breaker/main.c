9dcb07f752f69961bff14173ae5bb09e2b5e61c5
Unknown <200412@student.pwr.edu.pl>
2017-05-11 03:26:08 +0200

    Ogólnie to dodałem komentarze, program śmiga, a Fragles nadal terroryzuje swoją głupotą.

--- Lista 9/DES_Breaker/main.c
+++ Lista 9/DES_Breaker/main.c
@@ -30,6 +30,10 @@ int main(int argc, char** argv) {
     double timeStart;
     int i;
     
+    const int maxPasswordsFound = 1;
+    int passwordsFound;
+    
+    // Wszystkie mozliwe znaki
     const char* characters = "!\"#$%&'()*+,-./0123456789:;<=>?"
         "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_"
         "`abcdefghijklmnopqrstuvwxyz{|}~";
@@ -57,43 +61,65 @@ int main(int argc, char** argv) {
         char* zaszyfrowane;
         int max_dlugosc;
 
+        // Sprawdz argumenty wywolania
         if (validateExecutionArguments(argc, argv, zaszyfrowane, &max_dlugosc) != 0) {
             MPI_Abort(MPI_COMM_WORLD, rc);
             return (1);
         }
         zaszyfrowane = argv[1];
-
-        printf("Zaszyfrowane haslo: %s\n\n", zaszyfrowane);
         
+        // Liczba mozliwosci dla danej dlugosci
         long long numOfCases;
+        
+        // Zmienne przechowujace generowane hasla i ich hashe
         char* generatedPassword = (char*) calloc(max_dlugosc, sizeof (char));
         char* generatedHashPassword = (char*) calloc(13, sizeof (char));
         
+        // Liczba znalezionych hasel
+        int allPasswordsFound = 0;
+        
+        // Jezeli MASTER to jedyny proces
         if (worldSize == 1) {
             
+            // Generuj stringi o dlugosci od 1 do max_dlugosc
             for (i = 1; i <= max_dlugosc; i++) {
+                // Oblicz liczbe mozliwosci dla danej dlugosci
                 numOfCases = powlong((long long)strlen(characters), i);
                 
+                printf("----------Rozpoczynam wysylanie dla dlugosci %d----------\n", i);
+                
                 long long c;
+                // Wygeneruj kazda mozliwosc
                 for (c = 0; c < numOfCases; c++) {
                     generateNewPassword(generatedPassword, c, i, characters);
                     generatedHashPassword = crypt(generatedPassword, zaszyfrowane);
-//                    printf("Generuje: %s | ", generatedPassword);
-//                    printf("Ciag %d: %s |", j, crypt(generatedPassword, zaszyfrowane));
-//                    printf("Zaszyfrowane: %s\n", zaszyfrowane);
+                    
+                    // Jezeli haslo sie zgadza
                     if (!strcmp(generatedHashPassword, zaszyfrowane)) {
                         printf("Znalazlem haslo: %s | Czas trwania %lf\n", 
                                 generatedPassword, MPI_Wtime() - timeStart);
+                        allPasswordsFound++;
+                    }
+                    
+                    // Jezeli wystarczajaca ilosc hasel, zakoncz program
+                    if (allPasswordsFound >= maxPasswordsFound) {
+                        printf("Znaleziono wystarczajaco duzo hasel (%d)\n"
+                                "Koncze dzialanie programu.\n", allPasswordsFound);
+
+                        MPI_Finalize();
+                        return (0);
                     }
                 }
             }
         }
+        // Jezeli wiecej niz 1 proces
         else {           
             
             long long* startRowNumbers = (long long*) calloc(worldSize, sizeof (long long));
             long long* endRowNumbers = (long long*) calloc(worldSize, sizeof (long long));
             
             int p;
+            // Kazdemu procesowi wykonawczemu
             for (p = 1; p < worldSize; p++) {
                 // Wyslij zaszyfrowane haslo
                 MPI_Send(zaszyfrowane, 13, MPI_CHAR, p, 1, MPI_COMM_WORLD);
@@ -101,25 +127,23 @@ int main(int argc, char** argv) {
                 MPI_Send(&max_dlugosc, 1, MPI_INT, p, 2, MPI_COMM_WORLD);
             }
 
+            // Stringi o dlugosci od 1 do max_dlugosc
             for (i = 1; i <= max_dlugosc; i++) {
 
+                printf("----------Rozpoczynam wysylanie dla dlugosci %d----------\n", i);
+                
                 numOfCases = powlong((long long) strlen(characters), i);
 
-                // Wylicz poczatek i koniec
+                // Wylicz ile przypadkow na 1 proces
                 long long casesPerProcess = numOfCases / worldSize;
 
-//                printf("MASTER Cases per process: %lli\n\n", casesPerProcess);
-
-                // Wylicz wiersze startowe i koncowe blokow dla poszczegolnych procesow
+                // Wylicz przypadki startowe i koncowe blokow dla poszczegolnych procesow
                 int j;
                 for (j = 0; j < worldSize; j++) {
                     startRowNumbers[j] = j * casesPerProcess;
                     endRowNumbers[j] = startRowNumbers[j] + casesPerProcess - 1;
                 }
-                endRowNumbers[worldSize - 1] = numOfCases - 1;
-                
-//                printf("World rank %d, start: %lli\n", worldRank, startRowNumbers[0]);
-//                printf("World rank %d, end: %lli\n", worldRank, endRowNumbers[0]);
+                endRowNumbers[worldSize - 1] = numOfCases - 1;          
 
                 for (p = 1; p < worldSize; p++) {
                     // Wyslij numer poczatkowy
@@ -129,21 +153,49 @@ int main(int argc, char** argv) {
                 }
                 
                 long long c;
+                // Wygeneruj mozliwosci dla procesu 0
                 for (c = startRowNumbers[0]; c <= endRowNumbers[0]; c++) {
                     generateNewPassword(generatedPassword, c, i, characters);
                     generatedHashPassword = crypt(generatedPassword, zaszyfrowane);
-//                    printf("%d generuje: %s\n", worldRank, generatedPassword);
                     if (!strcmp(generatedHashPassword, zaszyfrowane)) {
                         printf("Znalazlem haslo: %s | Czas trwania %lf\n", 
                                 generatedPassword, MPI_Wtime() - timeStart);
                     }
                 }
+                
+                // Pobierz od innych procesow ilosc znalezionych hasel
+                for (p = 1; p < worldSize; p++) {
+                    MPI_Recv(&passwordsFound, 1, MPI_INT, p, 100 + i, MPI_COMM_WORLD, &status);
+                    allPasswordsFound += passwordsFound;
+                }
+                
+                // Jezeli ilosc znalezionych hasel jest wystarczajaca
+                if (allPasswordsFound >= maxPasswordsFound) {
+                    printf("Znaleziono wystarczajaco duzo hasel (%d)\n"
+                            "Koncze dzialanie programu.\n", allPasswordsFound);
+                    
+                    long long endValue = -1;
+                    // Wyslij sygnal zakonczajacy dzialanie procesow
+                    for (p = 1; p < worldSize; p++) {
+                        MPI_Send(&endValue, 1, MPI_LONG_LONG, p, 10 + i + 1, MPI_COMM_WORLD);  
+                    }
+                    
+                    MPI_Finalize();
+                    return (0);
+                }
             }
         }
+        
+        if (allPasswordsFound == 0) {
+            printf("Nie znaleziono zadnego pasujacego hasla.\n");
+        }
+        printf("Koncze dzialanie programu.\n");
     }        
     // Dla procesów wykonawczych
     else {
+        // Numer startowy badanych przypadkow
         long long startRowNumber;
+        // Numer koncowy badanych przypadkow
         long long endRowNumber;
         
         char* hashedPassword = (char*) calloc(13, sizeof(char));
@@ -153,32 +205,41 @@ int main(int argc, char** argv) {
         MPI_Recv(hashedPassword, 13, MPI_CHAR, 0, 1, MPI_COMM_WORLD, &status);
         MPI_Recv(&max_length, 1, MPI_INT, 0, 2, MPI_COMM_WORLD, &status);
         
-//        printf("WYKONAWCZY %d: Haslo: %s\n", worldRank, hashedPassword);
-//        printf("WYKONAWCZY %d: Length max: %d\n", worldRank, max_length);
-        
+        // Zmienne dla generowanego hasla i jego hasha
         char* generatedPassword = (char*) calloc(max_length, sizeof (char));
         char* generatedHashPassword = (char*) calloc(13, sizeof (char));
         
+        // Stringi o dlugosci od 1 do max_dlugosc
         for (i = 1; i <= max_length; i++) {
+            // Odbierz numer startowy
             MPI_Recv(&startRowNumber, 1, MPI_LONG_LONG, 0, 10 + i, MPI_COMM_WORLD, &status);
+            
+            // Jezeli -1 (znaleziono wystarczajaca ilosc hasel), zakoncz dzialanie
+            if (startRowNumber == -1) {
+                MPI_Finalize();
+                return(0);
+            }
+            
+            // Odbierz numer koncowy
             MPI_Recv(&endRowNumber, 1, MPI_LONG_LONG, 0, 50 + i, MPI_COMM_WORLD, &status);
-
-//            printf("World rank %d, start: %lli\n", worldRank, startRowNumber);
-//            printf("World rank %d, end: %lli\n", worldRank, endRowNumber);
+            
+            passwordsFound = 0;
 
             long long c;
+            // Sprawdz przypadki od numeru startowego do koncowego
             for (c = startRowNumber; c <= endRowNumber; c++) {
                 generateNewPassword(generatedPassword, c, i, characters);
                 generatedHashPassword = crypt(generatedPassword, hashedPassword);
-//                printf("%d generuje: %s\n", worldRank, generatedPassword);
                 if (!strcmp(generatedHashPassword, hashedPassword)) {
                     printf("Proces %d znalazl haslo: %s | Czas trwania %lf\n",
                         worldRank, generatedPassword, MPI_Wtime() - timeStart);
+                    passwordsFound++;
                 }
             }
+            
+            // Wyslij liczbe znalezionych hasel
+            MPI_Send(&passwordsFound, 1, MPI_INT, 0, 100 + i, MPI_COMM_WORLD);
         }
-        
-        // Wyslij info do mastera jak nie znajdziesz
     }
 
     MPI_Finalize();
@@ -186,6 +247,7 @@ int main(int argc, char** argv) {
     return (EXIT_SUCCESS);
 }
 
+// Sprawdzanie argumentow wywolania
 int validateExecutionArguments(int argc, char** argv, char* zaszyfrowane,
         int* max_dlugosc) {
 
@@ -212,6 +274,7 @@ int validateExecutionArguments(int argc, char** argv, char* zaszyfrowane,
     return (0);
 }
 
+// Funkcja potegi dla zmiennych typu long long
 long long powlong(long long base, int exp)
 {
     long long result = 1LL;
@@ -227,26 +290,36 @@ long long powlong(long long base, int exp)
     return result;
 }
 
+// Generowanie stringow
+// passwordVariable - zmienna przechowujaca haslo
+// index - badany przypadek (liczba zamieniana na string)
+// length - dlugosc generowanego hasla
+// characters - string przechowujacy znaki dozwolone w generowanych wyrazach
 void generateNewPassword(char* passwordVariable, long long index, 
         int length, const char* characters) {
 
     int charactersLength = strlen(characters);
+    
+    // Tablica indeksow kazdego ze znakow w stringu
     long long* permutationIndex = (long long*) calloc(length, sizeof (long long));
+    // Generowane haslo
     char* newPassword = (char*) calloc(length, sizeof (char));
     
     int i;
+    // Generowanie indeksow na podstawie dzielenia/modulo z indeksu
     permutationIndex[0] = index % charactersLength;
     for (i = 1; i < length; i++) {
         permutationIndex[i] = (index / (powlong(charactersLength, i))) % charactersLength;        
     }
 
+    // Zapisywanie do stringa
     for (i = 0; i < length; i++) {
         newPassword[i] = characters[permutationIndex[i]];        
     }
     
+    // Skopiowanie do podanej zmiennej zewnetrznej
     strcpy(passwordVariable, newPassword);
     
     free(permutationIndex);
     free(newPassword);
 }
-
