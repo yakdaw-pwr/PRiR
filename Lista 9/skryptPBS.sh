ffe1964394679f5da5399726fe02ae36a8d02cb0
Unknown <200412@student.pwr.edu.pl>
2017-05-23 15:32:26 +0200

    Poprawka skryptu PBS

--- Lista 9/skryptPBS.sh
+++ Lista 9/skryptPBS.sh
@@ -3,8 +3,9 @@
 #PBS -l walltime=01:00:00
 #PBS -j oe
 #PBS -l select=2:ncpus=6:mpiprocs=12
-cd ~/pbs
+
 mpicc /home/32b-g1/Lista9/main.c -o MPI-DesBreaker -lcrypt
+
 mpirun -n 1 --mca btl tcp,self --mca pml ob1 MPI-DesBreaker a8nNMN5joJMI6 8
 mpirun -n 2 --mca btl tcp,self --mca pml ob1 MPI-DesBreaker a8nNMN5joJMI6 8
 mpirun -n 8 --mca btl tcp,self --mca pml ob1 MPI-DesBreaker a8nNMN5joJMI6 8
@@ -19,4 +20,4 @@ mpirun -n 8 --mca btl tcp,self --mca pml ob1 MPI-DesBreaker a8bgkm3M5nUXQ 8
 
 mpirun -n 8 --mca btl tcp,self --mca pml ob1 MPI-DesBreaker a8jMSX/IQv.Pw 8
 mpirun -n 8 --mca btl tcp,self --mca pml ob1 MPI-DesBreaker a8.NqxJsLzjfM 8
-mpirun -n 8 --mca btl tcp,self --mca pml ob1 MPI-DesBreaker a8bgkm3M5nUXQ 8
+mpirun -n 8 --mca btl tcp,self --mca pml ob1 MPI-DesBreaker a82X4a/oSG3sQ 8
